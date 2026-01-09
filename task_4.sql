01e1dd241ddf67f1356a3a57158cd6a2f98fd31a
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-09 22:54:23 +0300

    Full Db description

---
+++ task_4.sql
@@ -0,0 +1,16 @@
+-- task_4.sql
+
+SELECT 
+    COLUMN_NAME AS `Column`,
+    COLUMN_TYPE AS `Data Type`,
+    IS_NULLABLE AS `Nullable`,
+    COLUMN_KEY AS `Key`,
+    COLUMN_DEFAULT AS `Default`,
+    EXTRA AS `Extra`
+FROM 
+    INFORMATION_SCHEMA.COLUMNS
+WHERE 
+    TABLE_SCHEMA = DATABASE() -- current database passed as argument
+    AND TABLE_NAME = 'BOOKS'
+ORDER BY 
+    ORDINAL_POSITION;
