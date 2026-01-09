9e33308aa3fef27954adba9fdbc443a3510ebf73
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-09 22:35:33 +0300

    Database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,14 +1,13 @@
 # MySQLServer.py
 
 import mysql.connector
-from mysql.connector import Error
 
 try:
     # Connect to MySQL server (without specifying a database)
     conn = mysql.connector.connect(
         host="127.0.0.1",
         user="root",
-        password="@Lonewolf92" 
+        password="@Lonewolf92"  # replace with your MySQL password
     )
 
     if conn.is_connected():
@@ -17,7 +16,7 @@ try:
         cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
         print("Database 'alx_book_store' created successfully!")
 
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error while connecting to MySQL: {e}")
 
 finally:
