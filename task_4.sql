0d8ff346b83ea63ec4d3d42743b460e5528659ec
syntax-elpadrino <eduardoeduuh@proton.me>
2026-01-09 22:56:00 +0300

    Full Db description

--- task_4.sql
+++ task_4.sql
@@ -10,7 +10,7 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS
 WHERE 
-    TABLE_SCHEMA = DATABASE() -- current database passed as argument
-    AND TABLE_NAME = 'BOOKS'
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'Books'
 ORDER BY 
     ORDINAL_POSITION;
